966c7a2c3510d09067f41c18e8c6a12ff3bf4eb3
idisin91 <idisin91@gmail.com>
2017-12-14 22:24:16 +0300

    added models.py

--- app.py
+++ app.py
@@ -14,13 +14,45 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 config = DevelopmentConfig()
 basedir = os.path.abspath(os.path.dirname(__file__))
-from models import Role, User, News
 app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = config.SQLALCHEMY_COMMIT_ON_TEARDOWN
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+    rus_name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class News(db.Model):
+    __tablename__ = 'news'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True, index=True)
+    text = db.Column(db.Text)
+    author = db.Column(db.String(64))
+    link = db .Column(db.Text)
+
+    def __repr__(self):
+        return '<Table %r>' % self.title
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 
 class AddUserForm(FlaskForm):
